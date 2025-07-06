cd5d460a60c133736d2b269ebf85a12a6eed4087
RicardoHerrera2004 <ricardo.herrera.acurio@udla.edu.ec>
2025-07-06 01:16:04 -0500

    Cambios realizados siguiendo los pasos de la guia de Microsoft Learn

--- People/People/Models/Person.cs
+++ People/People/Models/Person.cs
@@ -1,4 +1,10 @@
-﻿namespace People.Models;
+﻿using SQLite;
+namespace People.Models;
+[Table("personas")]
 public class Person
 {
+    [PrimaryKey, AutoIncrement]
+    public int Id { get; set; }
+    [MaxLength(250), Unique]
+    public string Name { get; set; }
 }
--- People/People/PersonRepository.cs
+++ People/People/PersonRepository.cs
@@ -1,4 +1,5 @@
-﻿using People.Models;
+﻿using SQLite;
+using People.Models;
 
 namespace People;
 
@@ -9,32 +10,35 @@ public class PersonRepository
     public string StatusMessage { get; set; }
 
     // TODO: Add variable for the SQLite connection
-
+    private SQLiteConnection conn;
     private void Init()
     {
-        // TODO: Add code to initialize the repository         
+        if (conn != null)
+            return;
+
+        conn = new SQLiteConnection(_dbPath);
+        conn.CreateTable<Person>();
     }
 
     public PersonRepository(string dbPath)
     {
-        _dbPath = dbPath;                        
+        _dbPath = dbPath;
     }
 
     public void AddNewPerson(string name)
-    {            
+    {
         int result = 0;
         try
         {
-            // TODO: Call Init()
+            // enter this line
+            Init();
 
             // basic validation to ensure a name was entered
             if (string.IsNullOrEmpty(name))
                 throw new Exception("Valid name required");
 
-            // TODO: Insert the new person into the database
-            result = 0;
-
-            StatusMessage = string.Format("{0} record(s) added (Name: {1})", result, name);
+            // enter this line
+            result = conn.Insert(new Person { Name = name });
         }
         catch (Exception ex)
         {
@@ -45,10 +49,10 @@ public class PersonRepository
 
     public List<Person> GetAllPeople()
     {
-        // TODO: Init then retrieve a list of Person objects from the database into a list
         try
         {
-            
+            Init();
+            return conn.Table<Person>().ToList();
         }
         catch (Exception ex)
         {
